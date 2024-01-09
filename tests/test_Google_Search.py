2993009c5b39d4a2ac1981c52e017428db8872f0
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2024-01-08 23:00:16 -0800

    Update test_Google_Search.py

--- tests/test_Google_Search.py
+++ tests/test_Google_Search.py
@@ -14,37 +14,38 @@ from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+def test_google():
+  chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                        # and if it doesn't exist, download it automatically,
+                                        # then add chromedriver to path
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1200,1200",
-    "--ignore-certificate-errors"
+  chrome_options = webdriver.ChromeOptions()    
+  # Add your options as needed    
+  options = [
+    # Define window size here
+      "--window-size=1200,1200",
+      "--ignore-certificate-errors"
  
-    #"--headless"
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+      #"--headless"
+      #"--disable-gpu",
+      #"--window-size=1920,1200",
+      #"--ignore-certificate-errors",
+      #"--disable-extensions",
+      #"--no-sandbox",
+      #"--disable-dev-shm-usage",
+      #'--remote-debugging-port=9222'
+  ]
 
-for option in options:
-    chrome_options.add_argument(option)
+  for option in options:
+      chrome_options.add_argument(option)
 
     
-driver = webdriver.Chrome(options = chrome_options)
-driver.get('https://www.google.com/')
-sleep(1)
-search = driver.find_element(By.CLASS_NAME, "gLFyf")
-search.send_keys('BaseMax' + Keys.RETURN)
-sleep(1)
-print(driver.title)
-sleep(2)
-driver.quit()
+  driver = webdriver.Chrome(options = chrome_options)
+  driver.get('https://www.google.com/')
+  sleep(1)
+  search = driver.find_element(By.CLASS_NAME, "gLFyf")
+  search.send_keys('Github' + Keys.RETURN)
+  sleep(1)
+  print(driver.title)
+  sleep(2)
+  driver.quit()
